73f7d130df1e7cfecb8fa7ad2f94fdfc64d6b864
EduCosta85 <59846713+EduCosta85@users.noreply.github.com>
2020-06-04 12:04:56 -0300

    created initial enviroment

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+
+import express from "express";
+import {AddressInfo} from "net";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+const server = app.listen(3000, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Spotnu running on: http://localhost:${address.port}`);
+  } else {
+    console.error(`Fail to start a server.`);
+  }
+});
